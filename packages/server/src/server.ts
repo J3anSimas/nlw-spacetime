25642caa2bb7d1cf25336f69d61a195d1969f2b3
j3ansimas <jeansimas16@gmail.com>
2023-05-23 18:05:27 -0300

    New Commit

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,13 +1,17 @@
 import fastify from "fastify";
+import cors from '@fastify/cors' 
 import { env } from "./env";
 import { PrismaClient } from "@prisma/client";
 import { memoriesRoutes } from "./routes/memories";
 
 const app = fastify();
+app.register(cors, {
+    origin: true
+});
 const prisma = new PrismaClient();
 
 app.register(memoriesRoutes);
-app.get("/users", async (req, res) => {
+app.get("/users", async () => {
     const users = await prisma.user.findMany();
     return users;
 });
